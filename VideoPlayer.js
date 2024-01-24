155e9418ebae1062ecb0f4d0454c413c39b1eaad
A Clever <aclever@mail.be>
2024-01-24 14:58:34 +0100

    set video player builder

--- VideoPlayer.js
+++ VideoPlayer.js
@@ -473,12 +473,27 @@ export class VideoPlayer {
 
     /**@private */
     buildVideoPlayer() {
-        console.warn("not building the player");
-        return;
-        let div = document.createElement("div");
-        div.setAttribute("aria-hidden", "true");
-        div.classList.add("controls");
-        div.innerHTML +=
+        // console.warn("not building the player");
+        // return;
+
+        let pulseContainer = document.createElement("div");
+        pulseContainer.classList.add("pulse_container");
+        pulseContainer.innerHTML =
+        `
+        <div class="pulse_play hidden">
+            <svg width="75" class="play_icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>
+        </div>
+        <div class="pulse_pause hidden">
+            <svg width="75" class="pause_icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"/></svg>
+        </div>
+        `
+
+        this.videoContainer.appendChild(pulseContainer);
+
+        let controls = document.createElement("div");
+        controls.setAttribute("aria-hidden", "true");
+        controls.classList.add("controls");
+        controls.innerHTML +=
             `
             <div class="progress_bar" role="slider" aria-valuemin="0%" aria-valuemax="100%"></div>
 
@@ -503,6 +518,6 @@ export class VideoPlayer {
                 </button>
             </div>
         `;
-        this.videoContainer.appendChild(div);
+        this.videoContainer.appendChild(controls);
     }
 }
